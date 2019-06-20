5fdf3a76aa3b2236974c21ced1e520fccaa11451
rathwaj <rathwaj@sheridancollege.ca>
2019-06-20 11:20:21 -0400

    practi

--- src/a3/EmployeeComparator.java
+++ src/a3/EmployeeComparator.java
@@ -14,6 +14,7 @@ public class EmployeeComparator implements Comparator<Employee>{
     @Override
     public int compare(Employee e1, Employee e2) {
         return Double.compare(e1.annualPay(), e2.annualPay());
+        // hello
     }
 }
 //end of code
